e02923f80af74cbc4e04c1acde53dcac0d5fd417
Tejasree8686 <guditejasree410@gmail.com>
2024-10-21 14:27:30 +0530

    Usin Destructuring method in props in React

--- App.js
+++ App.js
@@ -26,32 +26,58 @@ const cardstyle = {
         backgroundColor: "coral",
 }
 
-////Using Props ------------------------------->>>>>>>>>>>>>>
-// 1.Passing props to component is like passing arguments to function.
-// 2. The way of passing props to a component is ----------> <Headercomponent resname="meghanaaa" cuisine="Asian,italian"/> & props as an argument in the headercomponent function & wrap in{props.resname}= Meghana & {props.cuisine}=Asian,Italian    
-        const Restrntcardcomponent =(props) =>{
-                return(
-                        <div className="res-cardnew" style={cardstyle}>
-                                <img className="meghanaimg" src="https://b.zmtcdn.com/data/pictures/chains/1/50691/0435a03f4d2017a0a64d90b279c2fa63.jpg"/>
-                           <h3>{props.resname}</h3>
-                           <h4>{props.Cuisine}</h4>
-                           <h4>4.4 stars</h4>
-                           <h4>38 minutes</h4>
-                        </div>
-                )
-        }
+// using destructing:-One of the way---> Creating props in one object , & passes as an argument
+
+const Restrntcardcomponent =(props) =>{
+        const{resname ,Cuisine} = props;
+        return(
+                <div className="res-cardnew" style={cardstyle}>
+                        <img className="meghanaimg" src="https://b.zmtcdn.com/data/pictures/chains/1/50691/0435a03f4d2017a0a64d90b279c2fa63.jpg"/>
+                 <h3>{resname}</h3>
+                    <h4>{Cuisine}</h4>
+                   <h4>4.4 stars</h4>
+                   <h4>38 minutes</h4>
+                </div>
+        )
+} 
 
 const Bodycomponent = ()=>{
         return(
                 <div className="body">
                       <div className="searchbar">serachbar</div>
                       <div className="restarnt-cards">
-                           <Restrntcardcomponent resname="Meghana Fooo0ods" Cuisine="North IndianItalian"/>
-                          
+                           <Restrntcardcomponent resname="MeghanaaanaFo00ods" Cuisine="Sothhhhhhhhhh IndianItalian"/> 
                       </div>
                 </div>
         )
-}
+}   
+
+
+
+
+        // const Restrntcardcomponent =(props) =>{
+        //         return(
+        //                 <div className="res-cardnew" style={cardstyle}>
+        //                         <img className="meghanaimg" src="https://b.zmtcdn.com/data/pictures/chains/1/50691/0435a03f4d2017a0a64d90b279c2fa63.jpg"/>
+        //                    <h3>{props.resname}</h3>
+        //                    <h4>{props.Cuisine}</h4>
+        //                    <h4>4.4 stars</h4>
+        //                    <h4>38 minutes</h4>
+        //                 </div>
+        //         )
+        // }
+
+// const Bodycomponent = ()=>{
+//         return(
+//                 <div className="body">
+//                       <div className="searchbar">serachbar</div>
+//                       <div className="restarnt-cards">
+//                            <Restrntcardcomponent resname="Meghana Fooo0ods" Cuisine="North IndianItalian"/>
+                          
+//                       </div>
+//                 </div>
+//         )
+// }
 
 const Applayoutcomponent = () => {
   return (
