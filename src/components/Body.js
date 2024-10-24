b2250f0ee5e966626c7feb8aadf90cfd170b2302
Tejasree8686 <guditejasree410@gmail.com>
2024-10-24 16:16:12 +0530

    Episode -->5 --------->using Usestate Hook , to know DOM manipulation concept in React

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,32 @@
 import Restrntcard from "./Restrntcard";
 import restlist from "../utils/mockdata";
+import { useState } from "react";
+///React is good & efficient mainly in DOM Manipulation.
+// In React ,Dom manipulations makes in sink with data layer & UI layer.
 
 const Body = () => {
+  // LOcal state variable
+  const [ListofRestaurents, setListofRestaurents] = useState(restlist);
+//Normal javascript variable
+// let ListofRestaurents =[restlist]
+
   return (
     <div className="body">
       <div className="filter">
-        <button className="filter-btn" onClick={() => {
-          console.log("DRYYYYYYYNTgdfh;k")
-        }}>
-            Top rated Restaurants
-          </button >
-          <button onClick={() => console.log('Button clicked!')}>Click me!</button>
-        </div>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            let filteredlist = ListofRestaurents.filter(
+              (rest) => rest.data.avgRating > 4.6
+            );
+            setListofRestaurents(filteredlist);
+          }}
+        >
+          Top rated Restaurants
+        </button>
+      </div>
       <div className="restarnt-cards">
-        {restlist.map((restaurant) => (
+        {ListofRestaurents.map((restaurant) => (
           <Restrntcard key={restaurant.data.id} resdata={restaurant} />
         ))}
       </div>
